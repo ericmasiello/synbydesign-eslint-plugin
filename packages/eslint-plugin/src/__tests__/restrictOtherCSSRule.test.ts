d63c77d181c978ac7ae7f89f147179ea27f5dbbf
Eric Masiello <emasiello@vistaprint.com>
2023-04-02 20:39:51 -0400

    test: fix tests

--- packages/eslint-plugin/src/__tests__/restrictOtherCSSRule.test.ts
+++ packages/eslint-plugin/src/__tests__/restrictOtherCSSRule.test.ts
@@ -3,44 +3,73 @@ import path from "path";
 import { it } from "vitest";
 import { rule } from "../restrictAssetImport";
 
-it("should pass default configuration", () => {
+it("should match the default file extensions (e.g. `[.css, .scss, .module.css, .module.scss]`", () => {
   const tester = new RuleTester({
     parser: require.resolve("@typescript-eslint/parser"),
   });
 
+  const options = [
+    { cssExtensions: [".module.scss", ".module.css", ".css", ".scss"] },
+  ];
+
   tester.run("restrict-css-imports", rule, {
     valid: [
       {
-        code: `import './Foo.css';`,
+        code: `import './Foo.module.scss';`,
         filename: path.resolve("./Foo.tsx"),
+        options,
       },
       {
         code: `
           import 'some-library/dist/lib.css';
+          import './Foo.module.scss';
+        `,
+        filename: path.resolve("./Foo.tsx"),
+        options,
+      },
+      {
+        code: `
+          import 'some-library/dist/lib.css';
+          import * as scssStyles from './Foo.module.scss';
+          import * as cssStyles from './Foo.module.css';
+          import './Foo.scss';
           import './Foo.css';
         `,
         filename: path.resolve("./Foo.tsx"),
+        options,
       },
     ],
     invalid: [
       {
-        code: `import './styles/Other.css';`,
+        code: `
+          import 'some-library/dist/lib.css';
+          import { SomeComponent } from "./path/to/SomeComponent";
+          import * as scssStyles from './Bar.module.scss';
+          import * as cssStyles from './Bar.module.css';
+          import './Bar.scss';
+          import './Bar.css';
+      `,
         filename: path.resolve("./Foo.tsx"),
+        options,
         errors: [
           {
             message:
-              'Import path "./styles/Other.css" should match current file Foo.tsx as "./Foo.css"',
+              'Import path "./Bar.module.scss" should match current file Foo.tsx as "./Foo.module.scss"',
             type: "ImportDeclaration",
           },
-        ],
-      },
-      {
-        code: `import './otherDir/Foo.css';`,
-        filename: path.resolve("./Foo.tsx"),
-        errors: [
           {
             message:
-              'Import path "./otherDir/Foo.css" should match current file Foo.tsx as "./Foo.css"',
+              'Import path "./Bar.module.css" should match current file Foo.tsx as "./Foo.module.css"',
+            type: "ImportDeclaration",
+          },
+          {
+            message:
+              'Import path "./Bar.scss" should match current file Foo.tsx as "./Foo.scss"',
+            type: "ImportDeclaration",
+          },
+          {
+            message:
+              'Import path "./Bar.css" should match current file Foo.tsx as "./Foo.css"',
             type: "ImportDeclaration",
           },
         ],
@@ -121,81 +150,6 @@ it("should match a custom file extension (e.g. `.module.scss`", () => {
           {
             message:
               'Import path "./otherDir/Foo.module.scss" should match current file Foo.tsx as "./Foo.module.scss"',
-            type: "ImportDeclaration",
-          },
-        ],
-      },
-    ],
-  });
-});
-
-it("should match multiple custom file extensions (e.g. `[.css, .scss, .module.css, .module.scss]`", () => {
-  const tester = new RuleTester({
-    parser: require.resolve("@typescript-eslint/parser"),
-  });
-
-  const options = [
-    { cssExtensions: [".module.scss", ".module.css", ".css", ".scss"] },
-  ];
-
-  tester.run("restrict-css-imports", rule, {
-    valid: [
-      {
-        code: `import './Foo.module.scss';`,
-        filename: path.resolve("./Foo.tsx"),
-        options,
-      },
-      {
-        code: `
-          import 'some-library/dist/lib.css';
-          import './Foo.module.scss';
-        `,
-        filename: path.resolve("./Foo.tsx"),
-        options,
-      },
-      {
-        code: `
-          import 'some-library/dist/lib.css';
-          import * as scssStyles from './Foo.module.scss';
-          import * as cssStyles from './Foo.module.css';
-          import './Foo.scss';
-          import './Foo.css';
-        `,
-        filename: path.resolve("./Foo.tsx"),
-        options,
-      },
-    ],
-    invalid: [
-      {
-        code: `
-          import 'some-library/dist/lib.css';
-          import { SomeComponent } from "./path/to/SomeComponent";
-          import * as scssStyles from './Bar.module.scss';
-          import * as cssStyles from './Bar.module.css';
-          import './Bar.scss';
-          import './Bar.css';
-      `,
-        filename: path.resolve("./Foo.tsx"),
-        options,
-        errors: [
-          {
-            message:
-              'Import path "./Bar.module.scss" should match current file Foo.tsx as "./Foo.module.scss"',
-            type: "ImportDeclaration",
-          },
-          {
-            message:
-              'Import path "./Bar.module.css" should match current file Foo.tsx as "./Foo.module.css"',
-            type: "ImportDeclaration",
-          },
-          {
-            message:
-              'Import path "./Bar.scss" should match current file Foo.tsx as "./Foo.scss"',
-            type: "ImportDeclaration",
-          },
-          {
-            message:
-              'Import path "./Bar.css" should match current file Foo.tsx as "./Foo.css"',
             type: "ImportDeclaration",
           },
         ],
